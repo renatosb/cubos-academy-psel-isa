e0f6c4086cdb6fc46b0f37f692314291be318f3d
Renato Bittencourt <renato.sb92@gmail.com>
2021-03-11 18:05:44 -0300

    Questão 08

---
+++ emprestimo.js
@@ -0,0 +1,15 @@
+function solucao(ganhos, serasaScore, mesesParaPagar) {
+    let juros = 0;
+    if (serasaScore < 300) {
+        juros = 3;
+    } else {
+        if (serasaScore < 700) {
+            juros = 9;
+        } else {
+            juros = 15;
+        }
+    }
+
+    const parcela = ((ganhos * 3) + (ganhos * (juros / 100))) / mesesParaPagar;
+    console.log(parcela);
+}
